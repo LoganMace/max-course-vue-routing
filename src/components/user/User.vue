8300476f415e1b8d13056d1d5b45bdf57fd92d8e
Logan Mace <lmmace92@hotmail.com>
2018-10-16 11:11:04 -0500

    added conditional routes

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,16 +2,28 @@
     <div>
         <h1>The User Page</h1>
         <hr>
+        <!-- <p>Loaded ID: {{ $route.params.id }}</p> -->
         <button class="btn btn-primary" @click="navigateToHome">Go to Home</button>
+        <router-view></router-view>
     </div>
 
 </template>
 
 <script>
     export default {
+        // data() {
+        //     return {
+                
+        //     }
+        // },
+        // watch: {
+        //     '$route'(to, from) {
+        //         this.id = to.params.id;
+        //     }
+        // },
         methods: {
             navigateToHome() {
-                this.$router.push('/');
+                this.$router.push({ name: 'home' });
             }
         }
     }
